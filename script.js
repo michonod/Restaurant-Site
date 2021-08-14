9591ba5efbbf3611a7d879effca59e5d9b990c10
Mihail <majkl_david@hotmail.com>
2021-08-14 18:36:05 +0200

    nav changed and our work section done

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const toggleBtn = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-links");
+const navContainer = document.querySelector(".nav-container");
 
 for (let i = 0; i < navLinks.length; i++) {
   toggleBtn.addEventListener("click", () => {
     navLinks[i].classList.toggle("active");
   });
 }
+
+toggleBtn.addEventListener("click", () => {
+  navContainer.classList.toggle("nav-container-nav");
+});
